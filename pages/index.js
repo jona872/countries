8ddbef529ef59111879c6bdfd028f118adbae00f
jona872 <jona_872@hotmail.com>
2022-10-09 11:43:56 -0300

    search filtering

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,12 @@ import Layout from '../components/Layout/Layout';
 import Card from '../components/Card/Card';
 import axios from 'axios';
 import { useEffect, useState } from 'react'
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
 
 export default function Home({ countries }) {
-  const [countryList, setCountryList] = useState(countries);
-  
-  // const [selectList, setSelectList] = useState([]);
-  // countries.map((country) => {
-  //   if (!selectList.includes(country.region)) {
-  //     setSelectList([...selectList,country.region]);
-  //   }});
-  //   console.log(selectList);
+  const [countryList, setCountryList] = useState([]);
+  const [search, setSearch] = useState("");
+  const [selectFilter, setSelectFilter] = useState("");
 
 
   const vCard = countryList.map((country) => {
@@ -37,9 +32,50 @@ export default function Home({ countries }) {
     }
   });
 
+  function handleChange(event) {
+    setSearch(event.target.value);
+    // console.log(search);
+    // console.log("Cambio Search: " + search);
+  }
+
+  function handleSelectChange(event) {
+    setSelectFilter(event.target.value);
+  }
+
+  //handle searchBar
+  useEffect(() => {
+    if (search === "") {
+      setCountryList(countries);
+    } else {
+      const filteredCountriesByName = [...countries].filter((country) =>
+        country.name.common.toLowerCase().includes(search.toLowerCase())
+      );
+      setCountryList(filteredCountriesByName);
+    }
+
+  }, [search]);
+
+  //handle selectFilter
+  useEffect(() => {
+    if (selectFilter === "") { //Verify base on search and selectFilter
+      setCountryList(countries);
+    } else {
+      const filteredCountries = [...countries].filter((country) =>
+        country.region.includes(selectFilter)
+      );
+      // console.log("🚀 ~ filteredCountries", filteredCountries)
+      setCountryList(filteredCountries);
+    }
+
+  }, [selectFilter]);
+
+
+
+
+
 
   useEffect(() => {
-  
+
     if (typeof window !== "undefined") {
       var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
       var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
@@ -118,26 +154,23 @@ export default function Home({ countries }) {
 
           {/* SEARCH BAR */}
           <div className="flex flex-col justify-between gap-5 py-5 sm:flex-row sm:items-center sm:px-5">
-            <div>
-              <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
-              <div className="relative">
-                <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+              <div>
+                <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
+                <div className="relative">
+                  <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                    {/* SearchLogo */}
+                    <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
 
-                  <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-
+                  </div>
+                  <input type="search" id="default-search" name="default-search" className="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for a Country" required
+                    value={search} onChange={handleChange} />
                 </div>
-                <input type="search" id="default-search" className="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for a Country" required />
-                {/* <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button> */}
               </div>
-            </div>
 
             <div>
-              <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-md p-3 pr-10 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-md p-3 pr-10 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                value={search} onChange={handleSelectChange} >
                 <option defaultValue="0">Filter by region</option>
-                {/* <option value="US">United States</option>
-                <option value="CA">Canada</option>
-                <option value="FR">France</option>
-                <option value="DE">Germany</option> */}
                 {
                   vRegions.map((x, y) =>
                     <option key={y}>{x}</option>)

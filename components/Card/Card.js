cb751259fb02d3fc73d94987741caf491bd8a99b
jona872 <jona_872@hotmail.com>
2022-10-10 10:13:56 -0300

    ssr

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -44,37 +44,3 @@ export default function Card(props) {
     </>
   )
 }
-
-{/* <>
-            
-            <div className="max-w bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mb-2">
-            
-                <img className="rounded-t-lg" src="https://flagcdn.com/br.svg" alt="flag" />
-
-            
-                <div className="p-7">
-            
-                    <p className="mb-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Brazil</p>
-
-                    
-                    <div className="mb-3 font-bold text-gray-700 dark:text-gray-400">
-                        <p>
-                            Population: <span className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                                206.135.893
-                            </span>
-                        </p>
-                        <p>
-                            Region: <span className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                                Americas
-                            </span>
-                        </p>
-                        <p>
-                            Capital: <span className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                                Brasilia
-                            </span>
-                        </p>
-                    </div>
-                </div>
-            </div> 
-        </> */}
-
--- pages/[name].js
+++ pages/[name].js
@@ -1,28 +1,132 @@
 import React from 'react'
-import Layout from '../components/Layout/Layout'
+import Layout from '../components/Layout/Layout';
+import axios from 'axios';
+import Link from 'next/link';
 
 
+function parseCurrencies(currencies) {
+    //currencies: { EUR: Object { name: "Euro", symbol: "€" } }
+    let extract = Object.values(currencies)[0].name;
+    const output = (typeof extract !== "undefined") ? extract : "Not found";
+    return output;
+}
 
-export default function showCountry({ props }) {
-  console.log("🚀 ~ file: [name].js ~ line 7 ~ showCountry ~ props", props)
-  return (
-    <Layout>
-      <div className="grid place-items-center h-5/6" >
-        Country
-      </div>
-    </Layout>
-
-  )
+function parseLenguajes(languages) {
+    //lenguajes: deu: "German", fra: "French",  nld: "Dutch"
+    let extract = Object.values(languages).toString();
+    const output = (typeof extract !== "undefined") ? extract : "Not found";
+    return output;
 }
 
 
-// export const getServerSideProps = async (context) => {
-//   const res = await axios.put('http://0.0.0.0:3000/api/products/edit' + context.query.id);
-//   console.log(context);
+export default function showCountry({ props }) {
+    // console.log("🚀 ~ file: [name].js ~ line 7 ~ showCountry ~ props", props[0])
+    const country = props[0];
+    // console.log(Object.values(country.name.nativeName));
+    return (
+        <Layout>
+            <Link href="/">
+                <a>
+                    BACK
+                </a>
+            </Link>
+            {country.flags.svg}
+            <br />
+            {country.name.common}
+            <br />
+            {country.name.common}
+            <br />
+            {country.population}
+            <br />
+            {country.region}
+            <br />
+            {country.subregion}
+            <br />
+            {country.capital[0]}
+            <br />
+            {country.tld[0]}
+            <br />
+            Currencies: {parseCurrencies(country.currencies)}
+            <br />
+            Lenguajes: {parseLenguajes(country.languages)}
+            {/* {country.lenguajes} */}
+            {/* deu: "German"
+            fra: "French"
+            nld: "Dutch" */}
+            <br />
+            Borders
+            {/* {country.borders}
+            0: "FRA"
+            1: "DEU"
+            2: "LUX"
+            3: "NLD" */}
+            <br />
 
-//   return {
-//     props: {
 
+        </Layout>
+
+    )
+}
+
+export async function getServerSideProps(context) {
+    const { params } = context;
+    const { name } = params;
+    const res = await fetch(`https://restcountries.com/v3.1/name/${name}`);
+    const country = await res.json();
+
+      if (!country) {
+        return {
+          notFound: true,
+        }
+      }
+
+    return {
+        props: { props: country }, // will be passed to the page component as props
+    }
+}
+
+// // Working but loss data on refresh --------------------------------
+// export async function getStaticPaths() {
+//     //le pongo 1 por defecto, pero va generando todas dinamicamente la primera vez que entra
+//     return {
+//         paths: [{ params: { name: "Iceland" } }],
+//         fallback: true,
 //     }
-//   }
 // }
+
+// export async function getStaticProps(context) {
+//     //context = params, response, query
+//     const { params } = context;
+//     const { name } = params;
+
+//     const apiResponse = await fetch(`https://restcountries.com/v3.1/name/${name}`);
+
+//     if (apiResponse.ok) {
+//         const props = await apiResponse.json();
+
+//         return { props: { props } }
+//     }
+// }
+
+
+// export async function getServerSideProps(context) {
+//     // console.log("🚀 ~ file: [name].js ~ line 21 ~ getServerSideProps ~ context", context)
+//     const res = await axios.put('https://restcountries.com/v3.1/name/' + context.query.name);
+//     const country = await res.json();
+
+//     if (!country) {
+//         return {
+//             notFound: true,
+//         }
+//     }
+
+//     return {
+//         props: { country }, // will be passed to the page component as props
+//     }
+// }
+
+
+
+
+
+
